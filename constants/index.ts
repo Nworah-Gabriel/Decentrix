88d2a1c3f5ae33affcf9a937360b239e4ddd8c49
Chizihn <chizihn@gmail.com>
2025-05-31 16:16:51 +0100

    explorer frontend

--- constants/index.ts
+++
@@ -1,6 +0,0 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL as string;
-
-export const REDIRECT_URL =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:3000"
-    : process.env.NEXT_PUBLIC_REDIRECT_URL;
